73610db309e07eed85e2da52e84039188757412b
Dang Pham <thienvu453@gmail.com>
2020-07-05 17:28:12 +0700

    update total price

--- src/main/java/codegym/controller/CartController.java
+++ src/main/java/codegym/controller/CartController.java
@@ -6,12 +6,16 @@ import codegym.model.Product;
 import codegym.repository.ProductReposioty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @SessionAttributes("cart")
@@ -27,9 +31,10 @@ public class CartController {
     }
 
 
-    @GetMapping("/addcart/{id}")
-    public String addCart(@ModelAttribute("cart")HashMap<Long,Cart> cart, @PathVariable Long id){
 
+
+    @GetMapping("/addcart/{id}")
+    public ModelAndView addCart(@ModelAttribute("cart")HashMap<Long,Cart> cart, @PathVariable Long id){
         if(cart==null){
             cart= new HashMap<>();
         }
@@ -47,20 +52,33 @@ public class CartController {
                 cart.put(id,item);
             }
         }
+        ModelAndView mv = new ModelAndView("myCart");
+        mv.addObject("totalPrice",this.totalPrice(cart));
 
-        return "myCart";
+        return mv;
     }
 
     @GetMapping("/deletecart/{id}")
-    public String removeItem(@ModelAttribute("cart") HashMap<Long,Cart> cart, @PathVariable Long id){
+    public ModelAndView removeItem(@ModelAttribute("cart") HashMap<Long,Cart> cart, @PathVariable Long id){
         if(cart==null){
             cart =  new HashMap<>();
         }
         if(cart.containsKey(id)){
             cart.remove(id);
         }
-        return "myCart";
+        ModelAndView mv = new ModelAndView("myCart");
+        mv.addObject("totalPrice",this.totalPrice(cart));
+        return mv;
     }
+
+    public int totalPrice(HashMap<Long,Cart> mycart){
+        int total = 0;
+        for (Map.Entry<Long,Cart> list:mycart.entrySet()) {
+            total+= list.getValue().getProducts().getPrice()*list.getValue().getAmount();
+        }
+        return total;
+    }
+
 
 
 
